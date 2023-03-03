a2edc95c439bc09329902567fd1aa6a92d1deab3
Rizwan Ahmad <rizwan.ahmad.bhatti.1@gmail.com>
2023-03-03 17:45:31 +0500

    adding the shell script for cleaning everything

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Filename:    clean.sh
+#Description: Shell script to clean any Test_folders
+#             Prerequisties:-
+#             sudo -s
+#             source ./venv/bin/activate
+#              
+#Author:      21030005@lums.edu.pk
+
+rm -rf ./Test_*
